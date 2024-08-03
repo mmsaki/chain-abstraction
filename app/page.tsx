acbaa346d715f0080aafce75568b7b4afb01bb0d
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-03 13:05:27 -0500

    create passkeys credentials

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { createCred } from "./passkeys";
+import { addressToEIP3770 } from "./utils";
+
 export default function Home() {
   return (
     <main className="p-8">
-      <h1>Chain Abstraction</h1>
+      <h1 className="text-2xl font-bold">Chain Abstraction</h1>
+      <p>1. Create Credentials</p>
+      {}
+      <button className="border px-4 py-1" type="button" id="sign-in" onClick={createCred}>
+        Sign Up
+      </button>
+      <p>{addressToEIP3770("ETH", "0x000b")}</p>
     </main>
   );
 }
---
+++ app/passkeys.ts
@@ -0,0 +1,31 @@
+/*
+ * @notice: Creates passkey credentials
+ * @reuturn: PublicKeyCredential
+ * */
+export async function createCred(): Promise<PublicKeyCredential | undefined> {
+  if (!window.PublicKeyCredential) alert("Passkeys not supported on this Client/Platform/Browser!");
+
+  let publicKey = {
+    challenge: new Uint8Array([0]),
+    rp: { name: "chain-abstraction" },
+    user: { id: new Uint8Array([0]), name: "msaki", displayName: "Meek Msaki" },
+    pubKeyCredParams: [
+      { type: "public-key", alg: -7 },
+      { type: "public-key", alg: -257 },
+    ],
+    authenticatorSelection: { userVerification: "preferred" },
+    timeout: 300000,
+  };
+
+  await navigator.credentials
+    .create({ publicKey })
+    .then((creds) => {
+      console.log(creds);
+      return creds;
+    })
+    .catch((err) => {
+      alert("The operation either timed out or failed.");
+      console.error(err);
+    });
+  return;
+}
