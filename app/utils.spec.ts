893a24c637e331c268db13ed5cdccdee027ee3e5
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-03 17:28:12 -0500

    test for checksum address

--- app/utils.spec.ts
+++ app/utils.spec.ts
@@ -4,8 +4,8 @@ import { toHex } from "viem";
 test("test EIP3770 Chain-specific address", () => {
   const shortName = "ETH";
   // Test invalid checksum address
-  const invalidAddress = "0x04655832bcb0a9a0bE8c5AB71E4D311464c97Af5";
-  expect(() => addressToEIP3770(shortName, invalidAddress)).toThrow("Address is invalid");
+  const nonChecksumAddress = "0x04655832bcb0a9a0bE8c5AB71E4D311464c97Af5";
+  expect(addressToEIP3770(shortName, nonChecksumAddress)).toBe("ETH:0x04655832bcb0a9a0bE8c5AB71E4D311464c97AF5");
 
   // Test valid checksum address
   const validAddress = "0x04655832bcb0a9a0bE8c5AB71E4D311464c97AF5";
--- app/utils.ts
+++ app/utils.ts
@@ -1,4 +1,4 @@
-import { Hex, isAddress } from "viem";
+import { checksumAddress, Address, isAddress } from "viem";
 import { Octokit } from "@octokit/core";
 
 /**
@@ -10,14 +10,15 @@ import { Octokit } from "@octokit/core";
  *
  * @return {string} "shortName:address"
  */
-export function addressToEIP3770(shortName: string, address: Hex): string {
-  if (!isAddress(address)) throw new Error("Address is invalid, convert to checksum address");
+export function addressToEIP3770(shortName: string, address: Address): string {
+  address = checksumAddress(address);
+  if (!isAddress(address)) alert("Address is invalid");
 
   return shortName + ":" + address;
 }
 
 /**
- * GET request Github endpoint
+ * GET requet Superchain Token List
  * @notice: Reference for API https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
  *
  * We want the list of tokens available in the Superchain Token list
@@ -41,6 +42,7 @@ export async function getTokenList(owner: string, repo: string, path: string) {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   });
+
   const tokenList = JSON.stringify(
     res.data.map((el: any) => {
       return el["name"];
@@ -48,4 +50,37 @@ export async function getTokenList(owner: string, repo: string, path: string) {
   );
   console.log(tokenList);
   return tokenList;
+}
+
+/**
+ * GET request Github endpoint
+ * @notice: Read content of file of github file
+ *
+ * @param: {string} tokenName from OP Chain token list
+ *
+ * @return: tokenData see https://static.optimism.io/#adding-a-token-to-the-list
+ *          {"name": string, "symbol": string, "tokens": {"base": "0xB45E...", "optimism": "0xED32..."}}
+ */
+export async function getTokenData(tokenName: string) {
+  let owner = "ethereum-optimism";
+  let repo = "ethereum-optimism.github.io";
+  let path = "data";
+  const tokenList = await getTokenList(owner, repo, path);
+
+  if (!tokenList.includes(tokenName)) throw new Error(`${tokenName} not found in OP Chain list`);
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const tokenData = await octokit.request(`GET /repos/${owner}/${repo}/contents/${path}/${tokenName}`, {
+    owner: "OWNER",
+    repo: "REPO",
+    path: "PATH",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+
+  return tokenData;
 }
