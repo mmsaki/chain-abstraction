fec90d1975a8f0b41ee3e6e47d5aa0a1077e34fa
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-03 16:49:27 -0500

    convert address to EIP3770 convertible

---
+++ app/utils.spec.ts
@@ -0,0 +1,13 @@
+import { addressToEIP3770 } from "./utils";
+import { toHex } from "viem";
+
+test("test EIP3770 Chain-specific address", () => {
+  const shortName = "ETH";
+  // Test invalid checksum address
+  const invalidAddress = "0x04655832bcb0a9a0bE8c5AB71E4D311464c97Af5";
+  expect(() => addressToEIP3770(shortName, invalidAddress)).toThrow("Address is invalid");
+
+  // Test valid checksum address
+  const validAddress = "0x04655832bcb0a9a0bE8c5AB71E4D311464c97AF5";
+  expect(addressToEIP3770(shortName, validAddress)).toBe("ETH:0x04655832bcb0a9a0bE8c5AB71E4D311464c97AF5");
+});
---
+++ app/utils.ts
@@ -0,0 +1,16 @@
+import { Hex, isAddress } from "viem";
+/**
+ * @title EIP3770 Chain-specific address
+ * @notice Reference: https://eips.ethereum.org/EIPS/eip-3770
+ *
+ * @param  {string} `shortName` is mandatory and MUST be a valid chain short name from https://github.com/ethereum-lists/chains
+ * @param  {Hex}    `address`   is mandatory and MUST be a [ERC-55](/EIPS/eip-55) compatible hexadecimal address
+ *
+ * @return {string} "shortName:address"
+ */
+export function addressToEIP3770(shortName: string, address: Hex): string {
+  if (!isAddress(address)) throw new Error("Address is invalid, convert to checksum address");
+
+  return shortName + ":" + address;
+}
+
