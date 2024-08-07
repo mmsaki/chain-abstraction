10d09ceec7ccb4ec9c92a97da2e4a41cfe218072
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-06 19:23:39 -0500

    ERC3770 query parser and test

---
+++ app/utils.spec.ts
@@ -0,0 +1,5 @@
+test("parse Query correctly", () => {
+  // USDT address
+  let query = "metall2:0xdAC17F958D2ee523a2206206994597C13D831ec7";
+  expect(parseQuery(query)).toEqual({ operator: "metall2", query: "0xdAC17F958D2ee523a2206206994597C13D831ec7" });
+});
---
+++ app/utils.ts
@@ -0,0 +1,19 @@
+/**
+ * Parse an ERC3770 query and returns an object
+ *
+ * @param query - a ERC3770 string ex. "metall2:0xdAC17F958D2ee523a2206206994597C13D831ec7";
+ * @returns {operator: "", query: ""}
+ */
+export function parseQuery(query: string) {
+  let re = /(.+):([^\s]+)/;
+  let arr = query.match(re);
+
+  if (arr?.length == 3) {
+    return {
+      operator: arr[1],
+      query: arr[2],
+    };
+  } else {
+    return {};
+  }
+}
