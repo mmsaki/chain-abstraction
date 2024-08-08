a71229748ec90adc9a1a6a06ad8b5c68175e2e3c
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-08 15:44:00 -0500

    add response data as comment

--- app/depositStatus.ts
+++ app/depositStatus.ts
@@ -1,3 +1,8 @@
+// response:
+// {
+//   "fillStatus": "filled",
+//   "fillTxHash": "text"
+// }
 export async function getDepositStatus(originChainId: BigInt, depositId: BigInt) {
   let baseURL = `https://app.across.to/api`;
   let route = `/deposit/status`;
--- app/suggestedFees.ts
+++ app/suggestedFees.ts
@@ -1,5 +1,31 @@
 import { Address } from "viem";
 
+// ressponse:
+// {
+//   "totalRelayFee": {
+//     "pct": "376607094864283",
+//     "total": "376607094864283"
+//   },
+//   "relayerCapitalFee": {
+//     "pct": "100200000000000",
+//     "total": "100200000000000"
+//   },
+//   "relayerGasFee": {
+//     "pct": "276407094864283",
+//     "total": "276407094864283"
+//   },
+//   "lpFee": {
+//     "pct": "4552495218411721",
+//     "total": "4552495218411721"
+//   },
+//   "timestamp": "1708047000",
+//   "isAmountTooLow": false,
+//   "quoteBlock": "19237525",
+//   "spokePoolAddress": "0xe35e9842fceaCA96570B734083f4a58e8F7C5f2A",
+//   "exclusiveRelayer": "0x428AB2BA90Eba0a4Be7aF34C9Ac451ab061AC010",
+//   "exclusivityDeadline": "10"
+// }
+
 export async function getSuggestedFees(
   inputToken: Address,
   outputToken: Address,
