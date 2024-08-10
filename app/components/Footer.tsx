5238443be421d890c66c1aaba0b1262c9420c86e
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-10 18:13:43 -0500

    fix padding on small devices

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,15 +6,19 @@ import React from "react";
 function Footer() {
   const pathname = usePathname();
   return (
-    <div className="flex-initial h-16 w-full justify-between items-center gap-20 inline-flex text-zinc-600 p-5">
+    <div className="flex-initial h-16 w-full justify-between items-center gap-20 inline-flex text-zinc-600 xs:p-2 sm:p-5">
       <a
         href="/about"
-        className={`link ${pathname === "/about" ? "active text-gray-800 font-medium underline px-3" : "px-3"}`}>
+        className={`link ${
+          pathname === "/about" ? "active text-gray-800 font-medium underline xs:px-1 sm:px-3" : "xs:px-1 sm:px-3"
+        }`}>
         About
       </a>
       <a
         href="/feedback"
-        className={`link ${pathname === "/feedback" ? "active text-gray-800 font-medium underline px-3" : "px-3"}`}>
+        className={`link ${
+          pathname === "/feedback" ? "active text-gray-800 font-medium underline xs:px-1 sm:px-3" : "xs:px-1 sm:px-3"
+        }`}>
         Feedback
       </a>
     </div>
