f22c75864305d89bcfbc93b7d93d924777476f0b
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-10 14:38:59 -0500

    account sign in page

---
+++ app/icons/AccounIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function AccounIcon() {
+  return (
+    <svg
+      className="stroke-gray-75 light:stroke-gray-800 dark:stroke-gray-75"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M12.3333 13V11.6667C12.3333 10.9594 12.0524 10.2811 11.5523 9.78105C11.0522 9.28095 10.3739 9 9.66667 9H4.33333C3.62609 9 2.94781 9.28095 2.44771 9.78105C1.94762 10.2811 1.66667 10.9594 1.66667 11.6667V13M9.66667 3.66667C9.66667 5.13943 8.47276 6.33333 7 6.33333C5.52724 6.33333 4.33333 5.13943 4.33333 3.66667C4.33333 2.19391 5.52724 1 7 1C8.47276 1 9.66667 2.19391 9.66667 3.66667Z"
+        stroke-width="1.6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default AccounIcon;
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import AccountIcon from "../icons/AccounIcon";
 
 function SignIn() {
-  return <div className="flex flex-1 justify-center items-center flex-col">Sign In</div>;
+  return (
+    <div className="flex flex-1 justify-center items-center flex-col">
+      <div className="flex border rounded-lg p-8">
+        <form action="" method="post" className="flex flex-col gap-2">
+          <label htmlFor="username">Username</label>
+          <input
+            className="focus:outline-none border border-gray-200 rounded-md p-2 mb-2"
+            type="username"
+            placeholder="meek"
+          />
+
+          <button
+            className="flex items-center justify-center bg-gray-700 text-gray-75 py-2 rounded-lg hover:bg-gray-900"
+            type="submit">
+            Sign In
+            <span className="px-2">
+              <AccountIcon />
+            </span>
+          </button>
+          <a className="underline" href="">
+            Forget Password?
+          </a>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
