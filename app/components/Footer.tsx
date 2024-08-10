3d7822bb4292b7b5ac22347a9163f1a755216d06
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-10 16:58:47 -0500

    added client route states

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function Footer() {
+  const pathname = usePathname();
   return (
     <div className="flex-initial h-16 w-full justify-between items-center gap-20 inline-flex text-zinc-600 p-5">
-      <a href="/about" className="hover:underline">
+      <a
+        href="/about"
+        className={`link ${pathname === "/about" ? "active text-gray-800 font-medium underline px-3" : "px-3"}`}>
         About
       </a>
-      <a href="" className="hover:underline hover:cursor-not-allowed xs:opacity-0 sm:opacity-100">
-        How Siera works
-      </a>
-      <a href="/feedback" className="hover:underline">
+      <a
+        href="/feedback"
+        className={`link ${pathname === "/feedback" ? "active text-gray-800 font-medium underline px-3" : "px-3"}`}>
         Feedback
       </a>
     </div>
--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,48 +1,67 @@
+"use client";
+
 import React from "react";
-import Image from "next/image";
 import MenuIcon from "../icons/MenuIcon";
 import ArrowUpIcon from "../icons/ArrowUpIcon";
 import ArrowDownIcon from "../icons/ArrowDownIcon";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Navigation() {
+  const pathname = usePathname();
   return (
     <div className="flex-initial w-full h-16 justify-between items-center inline-flex">
-      <div className="justify-start items-center flex p-5 gap-5">
-        <a href="/" className="font-sans text-gray-800 hover:underline hover:cursor-pointer" aria-current="page">
+      <div className="justify-start items-center flex p-5 gap-5 font-sans text-gray-800">
+        <Link
+          href="/"
+          className={`link ${pathname === "/" ? "active text-gray-800 font-medium underline" : ""}`}
+          aria-current="page">
           Siera
-        </a>
+        </Link>
       </div>
-      <div className="w-full justify-end xs:hidden sm:flex" id="navbar-solid-bg">
+      <div className="w-full justify-end xs:hidden sm:flex  font-sans text-gray-800" id="navbar-solid-bg">
         <MenuIcon />
-        <a href="/deposit" className="flex justify-center items-center px-3 font-sans text-gray-800 hover:underline">
+        <Link
+          href="/deposit"
+          className={`link ${pathname === "/deposit" ? "active text-gray-800 font-medium underline px-3" : "px-3"}`}>
           Deposit
-        </a>
-        <a
+        </Link>
+        <Link
           href="/transfer"
-          className="flex justify-center items-center px-3 font-sans gap-1 text-gray-800 rounded hover:underline">
+          className={`link ${
+            pathname === "/transfer"
+              ? "active text-gray-800 font-medium underline flex justify-center items-center px-3 gap-1"
+              : "flex justify-center items-center px-3 gap-1"
+          }`}>
           Transfer
           <span>
             <ArrowUpIcon />
           </span>
-        </a>
-        <a
+        </Link>
+        <Link
           href="/receive"
-          className="flex justify-center items-center px-3 font-sans gap-1 text-gray-800 rounded hover:underline">
+          className={`link ${
+            pathname === "/receive"
+              ? "active text-gray-800 font-medium underline flex justify-center items-center px-3 gap-1"
+              : "flex justify-center items-center px-3 gap-1"
+          }`}>
           Receive
           <span>
             <ArrowDownIcon />
           </span>
-        </a>
-        <a
+        </Link>
+        <Link
           href="/create-account"
-          className="flex justify-center items-center px-3 font-sans text-gray-800 rounded hover:underline">
+          className={`link ${
+            pathname === "/create-account" ? "active text-gray-800 font-medium underline px-3" : "px-3"
+          }`}>
           Create Account
-        </a>
-        <a
+        </Link>
+        <Link
           href="/sign-in"
-          className="flex justify-center items-center px-3 font-sans text-gray-800 rounded hover:underline">
+          className={`link ${pathname === "/sign-in" ? "active text-gray-800 font-medium underline px-3" : "px-3"}`}>
           Sign In
-        </a>
+        </Link>
       </div>
     </div>
   );
