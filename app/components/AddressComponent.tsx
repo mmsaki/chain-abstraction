2c68fad54cac3199702113e26cf2594747bf3eb3
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-08-12 12:46:58 -0500

    responsive deposite component

--- app/components/AddressComponent.tsx
+++ app/components/AddressComponent.tsx
@@ -6,7 +6,7 @@ function AddressComponent({ props }: any) {
   return (
     <div className="flex flex-col">
       <label htmlFor={props.label}>{props.label}</label>
-      <div className="relative flex flex-1 items-center gap-2 xs:text-[11px] md:text-sm">
+      <div className="relative flex flex-1 items-center gap-2 xs:text-[11px] md:text-[11px] lg:text-sm">
         <input
           className="flex-1 focus:outline-none border border-gray-200 rounded-md p-2 mb-2"
           type="text"
@@ -16,7 +16,7 @@ function AddressComponent({ props }: any) {
         />
         <a
           href="/deposit"
-          className="absolute inset-y-0 end-12 mb-2 pe-0 flex items-center hover:cursor-pointer xs:hidden sm:flex">
+          className="absolute inset-y-0 end-12 mb-2 pe-0 flex items-center hover:cursor-pointer xs:hidden md:flex">
           <QRCodeIcon />
         </a>
         <div className="flex-none hover:cursor-pointer">
--- app/deposit/page.tsx
+++ app/deposit/page.tsx
@@ -7,7 +7,7 @@ import { QRCode } from "react-qrcode-logo";
 function Deposit() {
   return (
     <div className="flex flex-1 justify-center items-center flex-col">
-      <div className="flex border rounded-lg p-6 flex-col gap-2 xs:w-[390px] sm:w-[400px] md:w-[450px]">
+      <div className="flex border rounded-lg p-6 flex-col gap-2 xs:w-[390px] sm:w-[400px] md:w-[450px] lg:w-[600px]">
         <AddressComponent props={{ label: "Deposit", placeholder: "eth:0x04655832bcb0a9a0bE8c5AB71E4D311464c97AF5" }} />
         <div className="flex justify-center">
           <QRCode
